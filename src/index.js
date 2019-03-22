1abd191e46092917797856c29ac12962c22cfb47
maryna-js <rastargueva.m@gmail.com>
2019-03-22 13:23:55 +0300

    Feat: brackets parenthesis implementation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+  let current;
+  let searchItem = {
+        "(": ")", 
+        "[": "]", 
+        "{": "}", 
+        
+      };
+                    
+  for (let i = 0; i < str.length; i++) {
+    current = str[i]; 
+    
+    if (current === '(' || current === '[' || current === '{') {
+      stack.push(current);
+      
+    } else if (current === ')' ||  current === ']' || current === '}') {
+      let lastBracket = stack.pop();
+      
+      if (searchItem[lastBracket] !== current) { 
+      
+        return false; 
+      } 
+      
+    }
+  }
+  
+  return stack.length === 0; 
 }
