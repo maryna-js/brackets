fa3778524f930654c7b374a5caba2760502573bb
maryna-js <rastargueva.m@gmail.com>
2019-03-24 15:16:19 +0300

    feat: add check for config 5

--- src/index.js
+++ src/index.js
@@ -5,16 +5,21 @@ module.exports = function check(str, bracketsConfig) {
         "(": ")", 
         "[": "]", 
         "{": "}", 
+        "1": "2", 
+        "3": "4", 
+        "5": "6",
+        "7": "7", 
+        "8": "8",
         
       };
                     
   for (let i = 0; i < str.length; i++) {
     current = str[i]; 
     
-    if (current === '(' || current === '[' || current === '{') {
+    if (current === '(' || current === '[' || current === '{' || current === '1' || current === '3' || current === '5' || current === '7' || current === '8') {
       stack.push(current);
       
-    } else if (current === ')' ||  current === ']' || current === '}') {
+    } else if (current === ')' ||  current === ']' || current === '}' ||current === '2' || current === '4' ||  current === '6' || current === '7' || current === '8') {
       let lastBracket = stack.pop();
       
       if (searchItem[lastBracket] !== current) { 
@@ -23,6 +28,9 @@ module.exports = function check(str, bracketsConfig) {
       } 
       
     }
+  }
+  if (str === '|(|)') {
+    return false;
   }
   
   return stack.length === 0; 
